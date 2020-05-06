9c286bb09876008c5d3779df987eb5d9c6723edf
didanednya <dida.nendya@gmail.com>
2020-05-06 14:01:23 +0700

    game
    2D platform
    dan pilah sampah

---
+++ 2DPlatform/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public int total_coin = 0; // digunakan untuk mencatat coin
+	public Text coinLabel; // Digunakan untuk menampilkan coin
+
+	private static GameController instance;
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddCoin(){
+		total_coin++;
+		coinLabel.text = "Coin: " + total_coin.ToString ();
+	}
+
+	public static GameController GetInstance(){
+		return instance;
+	}
+}
